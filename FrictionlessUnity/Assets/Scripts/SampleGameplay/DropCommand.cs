014cca10a9ce38386600b6af55301edd47fb6d91
Rodney S. Foley <CreepyGnome@users.noreply.github.com>
2020-10-11 18:25:13 -0700

    testing UPM via git

--- FrictionlessUnity/Assets/Scripts/SampleGameplay/DropCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DropCommand
-{
-	public float Force { get; set; }
-}
--- FrictionlessUnity/Assets/Scripts/SampleGameplay/GameConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using Frictionless;
-
-public class GameConfig : MonoBehaviour
-{
-}
